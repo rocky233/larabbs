9bd1abf1c700971b363fc391aeddee4487ddde79
zhaonan <zhaonan420@qq.com>
2019-09-16 16:22:43 +0800

    用户和数据填充

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -1,17 +1,28 @@
 <?php
 
-use Illuminate\Database\Seeder;
+use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
+use Illuminate\Database\Seeder;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
-        });
+        $user_ids = User::all()->pluck('id')->toArray();
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        $faker = app(Faker\Generator::class);
+
+
+        $topics = factory(Topic::class)
+            ->times(100)
+            ->make()
+            ->each(function ($topic, $index) use ($faker, $user_ids, $category_ids) {
+                $topic->user_id = $faker->randomElement($user_ids);
+                $topic->category_id = $faker->randomElement($category_ids);
+            });
+
 
         Topic::insert($topics->toArray());
     }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 获取实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像数据
+        $avatars = [
+            'http://file.qqtouxiang.com/nvsheng/2019-09-12/9648a13f6ddb46c7608aef4ba70c2cc4.jpeg',
+            'http://file.qqtouxiang.com/nvsheng/2019-09-12/489ebb5ef31708935399a290bd24fdb2.jpeg',
+            'http://file.qqtouxiang.com/nvsheng/2019-09-12/0e1a38002170adb489b688d1de831e1f.jpeg',
+            'http://file.qqtouxiang.com/qinglv/2019-09-10/77ac78cffe768c6739c9ca9b6d06b0cd.jpg',
+            'http://file.qqtouxiang.com/qinglv/2019-09-10/d0b32f43226188d8ed65ceb19178011d.jpg',
+            'http://file.qqtouxiang.com/qinglv/2019-09-10/77ac78cffe768c6739c9ca9b6d06b0cd.jpg',
+        ];
+
+        // 生成数据合集
+        $users = factory(User::class)->times(10)->make()->each(function ($user, $index) use ($faker, $avatars) {
+            $user->avatar = $faker->randomElement($avatars);
+        });
+
+        // 数据隐藏
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        $user = User::find(1);
+        $user->name = 'admin';
+        $user->email = "admin@qq.com";
+        $user->avatar = "http://cdn.duitang.com/uploads/item/201410/16/20141016202155_5ycRZ.thumb.700_0.jpeg";
+        $user->save();
+    }
+}
