082a9eacf8fd14eb0a1bf51966e6461cb4688573
zhaonan <zhaonan420@qq.com>
2019-09-16 11:20:42 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 //用户中心
 Route::resource('users', 'UsersController',['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
