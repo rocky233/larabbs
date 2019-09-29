e61cbd655608eb878213e7459418146157a77da8
zhaonan <zhaonan420@qq.com>
2019-09-29 16:53:16 +0800

    消息通知列表

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,5 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 
 
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+
+Route::resource('notifications', 'NotificationController', ['only' => ['index']]);
